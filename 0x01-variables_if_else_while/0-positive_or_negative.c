59d5d4800ae2c84badd1a32cdd4184e92ec0caf0
BityaFev <fevenbe01@gmail.com>
2022-12-11 00:24:25 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,10 +8,8 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -24,6 +22,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }	
