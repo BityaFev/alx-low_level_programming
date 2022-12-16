dc0646044a49302c74cf672d2642c27435315b15
BityaFev <fevenbe01@gmail.com>
2022-12-16 07:38:31 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,13 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 48; x < 58; x++)
-	
-		if (ch != 50 && ch != 52)
+	for (x = 0; x <= 9; x++)
+	{
+		if ((x == 2) || (x == 4))
+			continue;
+		
+		else
 			_putchar(x + '0');
+}
 		_putchar('\n');
 }
