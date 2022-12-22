18e2ce7586fb28b5b80e0dc7935dfceb4b47ecde
BityaFev <fevenbe01@gmail.com>
2022-12-22 10:00:37 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,12 @@
+# include "main.h"
+
+/**
+ * rot13 - encode a string
+ * @str: the string
+ * Return: the encoded string
+ */
+char *rot13(char *str)
+{
+	int index1, index2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+       	
