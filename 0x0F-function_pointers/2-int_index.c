ebff230f6ac4630ce474b15af82fe9d74e6964a8
BityaFev <fevenbe01@gmail.com>
2023-01-19 03:50:51 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for integer
+ * @array: array
+ * @size: size
+ * @cmp: comparing pointer
+ * Return: index of the first elmt does not return 0 or -1 if match is found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+			return (i);
+	}
+}
