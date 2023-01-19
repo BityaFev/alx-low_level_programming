1c83f463ceadd771e0e2b728c50bbbfba388c740
BityaFev <fevenbe01@gmail.com>
2023-01-19 03:53:53 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,6 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
-			return (i);
+		{
+			if (cmp(array[i] != 0)
+					return (i);
+					}
 	}
+	return (-1);
 }
